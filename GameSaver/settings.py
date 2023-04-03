14d56ff6dd6030801ab021212e0a839d4dd0dd8b
George Trujillo <georgeMike2030@outlook.com>
2023-04-03 14:27:55 -0500

    feat:add_static_url_and_bulmaframework

--- GameSaver/settings.py
+++ GameSaver/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static/css/'),)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
